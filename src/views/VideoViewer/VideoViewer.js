5cf4d2233c78ece660e094ccf4d8143f25d2e7d7
SeungHyun Kim <k10130219@gmail.com>
2021-12-30 12:18:27 +0900

    Adjust FrameRate

--- src/views/VideoViewer/VideoViewer.js
+++ src/views/VideoViewer/VideoViewer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useLayoutEffect, useRef, useState} from "react";
 import {createUseStyles} from "react-jss";
 
 const useStyles = createUseStyles({
@@ -6,19 +6,73 @@ const useStyles = createUseStyles({
         display: "flex",
         flexDirection: "column",
     },
+    canvasVideo: {
+        width: "30%"
+    }
 });
-
+const FPS = 1000 / 33.333333333;
 
 const VideoViewer = props => {
     const styles = useStyles();
+    const videoRef = useRef();
+    const canvasRef = useRef();
+    const [size, setSize] = useState({width: 0, height: 0});
+    const lastCalledRef = useRef();
+
+    const draw = () => {
+        if (canvasRef.current && videoRef.current) {
+            let fps = 0;
+            const ctx = canvasRef.current.getContext("2d");
+
+            ctx.drawImage(videoRef.current, 0, 0, videoRef.current.videoWidth / 3, videoRef.current.videoHeight / 3)
+            if (lastCalledRef.current) {
+                fps = (performance.now() - lastCalledRef.current) / 1000
+            }
+            ctx.font = '48px serif';
+            ctx.fillStyle = "red"
+            ctx.fillText(parseInt(1 / fps) + " FPS", 20, 50)
+            // console.log(`FPS : ${fps} fps`)
+            lastCalledRef.current = performance.now();
+            setTimeout(() => {
+                window.requestAnimationFrame(draw)
+            }, FPS)
+
+        }
+    }
+
+    useLayoutEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.addEventListener("canplaythrough", draw)
+            videoRef.current.addEventListener("canplay", () => {
+                if (canvasRef.current && videoRef.current) {
+                    setSize({
+                        width: videoRef.current.videoWidth / 3,
+                        height: videoRef.current.videoHeight / 3
+                    })
+                }
+            })
+        }
+    }, []);
     return (
         <div className={styles.root}>
             <a href={"/"}>{"Back"}</a>
-            <video width={500} controls autoPlay={true}>
+            <h2>
+                {"Video Tag"}
+            </h2>
+            <video ref={videoRef} width={500} controls autoPlay={true}>
                 <source src={"http://localhost:3001/assets/videos/test_video.mp4"} type="video/mp4"/>
                 {/*<source src={"http://localhost:3001/assets/videos/Sample-Video-File-For-Testing.mp4"} type="video/mp4"/>*/}
                 {"Your browser does not support the video tag."}
             </video>
+            <h2>
+                {"Canvas Tag"}
+            </h2>
+            <div>
+                <canvas
+                    width={size.width}
+                    height={size.height} ref={canvasRef}/>
+            </div>
+
         </div>
     )
 }
